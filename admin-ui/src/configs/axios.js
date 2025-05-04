3320f2b3f5f10c8e638b43b16fddeea6095f42d7
Tuyen Dang <dptuyenn1@gmail.com>
2025-05-04 23:49:08 +0700

    feat (config): add custom axios

---
+++ admin-ui/src/configs/axios.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: "http://localhost:8080/api",
+});
+
+instance.interceptors.response.use(
+  function (response) {
+    return response.data;
+  },
+  function (error) {
+    return error.response.data;
+  }
+);
+
+export default instance;
