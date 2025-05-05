9d3dea3f3ddff648e9310ba5d4e210fb5eab7abc
Tuyen Dang <dptuyenn1@gmail.com>
2025-05-05 19:55:42 +0700

    chore: add fetch api event

--- admin-ui/src/pages/Home/index.jsx
+++ admin-ui/src/pages/Home/index.jsx
@@ -1,7 +1,8 @@
-import { AUTH_REDUCER_TYPES } from "@/helpers/constants";
+import { authAPI } from "@/configs/axios";
+import { AUTH_REDUCER_TYPES, END_POINTS } from "@/helpers/constants";
 import useAuth from "@/hooks/useAuth";
-import { useEffect } from "react";
-import { Button } from "react-bootstrap";
+import { useEffect, useState } from "react";
+import { Button, Toast, ToastContainer } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 
 function Home() {
@@ -21,6 +22,16 @@ function Home() {
     });
   }
 
+  const [showToast, setShowToast] = useState(false);
+  const [message, setMessage] = useState("");
+
+  async function handleFetchAPI() {
+    const response = await authAPI().get(END_POINTS["hello"]);
+
+    setMessage(response);
+    setShowToast(true);
+  }
+
   return (
     <main>
       <section className="d-flex flex-column align-items-center gap-3 py-3">
@@ -28,13 +39,29 @@ function Home() {
 
         <div className="shadow-lg p-3 mb-5 bg-body rounded w-50">
           <div className="d-flex justify-content-center gap-3">
-            <Button variant="warning">Fetch API</Button>
+            <Button variant="warning" onClick={handleFetchAPI}>
+              Fetch API
+            </Button>
             <Button variant="danger" onClick={handleLogout}>
               Logout
             </Button>
           </div>
         </div>
       </section>
+
+      <ToastContainer position="top-end" className="mt-3 me-3">
+        <Toast
+          onClose={() => setShowToast(false)}
+          show={showToast}
+          delay={3000}
+          autohide
+        >
+          <Toast.Header>
+            <strong className="me-auto text-success">Message</strong>
+          </Toast.Header>
+          <Toast.Body>{message}</Toast.Body>
+        </Toast>
+      </ToastContainer>
     </main>
   );
 }
