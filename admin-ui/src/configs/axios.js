f227cd75a08cb4a8f6c1a277f369763790f6c0a4
Tuyen Dang <dptuyenn1@gmail.com>
2025-05-05 11:48:01 +0700

    feat (axios): add auth api

--- admin-ui/src/configs/axios.js
+++ admin-ui/src/configs/axios.js
@@ -1,16 +1,21 @@
 import axios from "axios";
+import Cookies from "js-cookie";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "http://localhost:8080",
 });
 
-instance.interceptors.response.use(
-  function (response) {
-    return response.data;
-  },
-  function (error) {
-    return error.response.data;
-  }
-);
+const accessToken = Cookies.get("accessToken");
+
+instance.interceptors.response.use(function (response) {
+  return response.data;
+});
+
+function authAPI() {
+  instance.defaults.headers.common["Authorization"] = `Bearer ${accessToken}`;
+
+  return instance;
+}
 
 export default instance;
+export { authAPI };
